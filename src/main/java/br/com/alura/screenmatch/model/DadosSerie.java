43bbc7e193a4ee837afd1bfff042dce7e790dc86
yanocosta02 <yancosta2002@gmail.com>
2026-03-10 01:46:00 -0300

    conectando ao banco

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,17 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+//não vai precisar manipular dados, apenas representar, por isso o uso do record
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonAlias("Genre") String genero,
+                         @JsonAlias("Actors") String atores,
+                         @JsonAlias("Poster") String poster,
+                         @JsonAlias("Plot") String sinopse) {
+
+
+}
